365c91d14b0f35dcc046a2e9fa06ea9fbb3fe7bc
Dustin Cooper <dgalcoop@gmail.com>
2016-04-11 13:11:24 -0600

    commit to transfer work to mac

--- js/photos-gallery.js
+++ js/photos-gallery.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 		autoplay: true,
 		autoplaySpeed: 7500,
 		slidesToScroll: 3,
-		arrows: true
+		prevArrow: '<div class="arrow carousel-prev"><i class="fa fa-chevron-circle-left"></i></div>',
+  	nextArrow: '<div class="arrow carousel-next"><i class="fa fa-chevron-circle-right"></i></div>'
 	});
 });
