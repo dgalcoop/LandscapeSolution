57b1a3610264aa47d47d8a3f1469e541796f303f
Amanda Saffer <amandasaffer@gmail.com>
2016-04-15 16:52:08 -0600

    override background color, fix carousel widths, better testimonial box, clearfix mixin update

--- js/photos-gallery.js
+++ js/photos-gallery.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
 		autoplay: true,
 		autoplaySpeed: 7500,
 		slidesToScroll: 3,
+		variableWidth: true,
 		prevArrow: '<div class="arrow carousel-prev"><i class="fa fa-chevron-circle-left"></i></div>',
   	nextArrow: '<div class="arrow carousel-next"><i class="fa fa-chevron-circle-right"></i></div>'
 	});
